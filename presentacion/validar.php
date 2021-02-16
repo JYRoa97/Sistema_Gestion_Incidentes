00c487efd7fdf09405ba91f8c6d7ea3c6211e069
Diego <draconreyes@gmail.com>
2021-02-16 11:46:05 -0500

    Se creo parche de seguridad

--- presentacion/validar.php
+++ presentacion/validar.php
@@ -1,25 +1,31 @@
 <?php
-    $correo=$_POST['correo'];
-    $clave=$_POST['clave'];
-    $administrador= new administrador("","","","","",$correo,$clave);
-    $usuario= new usuario("","","","","",$correo,$clave);
-    $tecnico = new tecnico("","","","","",$correo,$clave);
-    if($administrador->autenticar()){
-        $_SESSION['id']=$administrador->traerid();
-        $direccion=base64_encode("presentacion/administrador/seccion.php");
-        header("Location: index.php?pid=".$direccion);
-    }else if($usuario->autenticar()){
-        $_SESSION['id']=$usuario->traerid();
-        $direccion=base64_encode("presentacion/usuario/seccion.php");
-        header("Location: index.php?pid=".$direccion);
-    }else if($tecnico->autenticar()){
-        $_SESSION['id']=$tecnico->traerid();
-        $direccion=base64_encode("presentacion/tecnico/seccion.php");
-        header("Location: index.php?pid=".$direccion);
-
+    if(isset($_SESSION['id'])){
+        session_destroy ();
+        include "presentacion/inicio.php";
     }else{
-        header("Location: index.php");
+        $correo=$_POST['correo'];
+        $clave=$_POST['clave'];
+        $administrador= new administrador("","","","","",$correo,$clave);
+        $usuario= new usuario("","","","","",$correo,$clave);
+        $tecnico = new tecnico("","","","","",$correo,$clave);
+        if($administrador->autenticar()){
+            $_SESSION['id']=$administrador->traerid();
+            $direccion=base64_encode("presentacion/administrador/seccion.php");
+            header("Location: index.php?pid=".$direccion);
+        }else if($usuario->autenticar()){
+            $_SESSION['id']=$usuario->traerid();
+            $direccion=base64_encode("presentacion/usuario/seccion.php");
+            header("Location: index.php?pid=".$direccion);
+        }else if($tecnico->autenticar()){
+            $_SESSION['id']=$tecnico->traerid();
+            $direccion=base64_encode("presentacion/tecnico/seccion.php");
+            header("Location: index.php?pid=".$direccion);
+
+        }else{
+            header("Location: index.php");
+        }
     }
+
 
 ?>
 
